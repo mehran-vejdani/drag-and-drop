59e8401675bc73b93ce934ad417256e1fc1d7344
“mehran <mehran.vejdanii@gmail.com>
2023-04-25 00:22:01 +0430

    add dragleave function

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const image = document.querySelector(".image");
 boxes.forEach((box) => {
   box.addEventListener("dragover", (e) => {
     e.preventDefault();
+    box.classList.add("hoverd");
+  });
+  box.addEventListener("dragleave", (e) => {
+    box.classList.add("hoverd");
   });
 });
